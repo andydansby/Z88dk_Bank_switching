b7bfd81f80328adab42525aa21ea20858363e33e
Andrew Dansby <andydansby@gmail.com>
2025-08-05 06:09:12 -0400

    Add files via upload

---
+++ gfx/compressGFX.bat
@@ -0,0 +1,14 @@
+zx0 -f ram0_contended.scr ram0_contended_gfx.zx0
+zx0 -f ram0_uncontended.scr ram0_uncontended_gfx.zx0
+zx0 -f ram1.scr ram1_gfx.zx0
+zx0 -f ram3.scr ram3_gfx.zx0
+zx0 -f ram4.scr ram4_gfx.zx0
+zx0 -f ram6.scr ram6_gfx.zx0
+
+
+move "ram0_contended_gfx.zx0" "..\CONTENDED\"
+move "ram0_uncontended_gfx.zx0" "..\RAM0\"
+move "ram1_gfx.zx0" "..\RAM1\"
+move "ram3_gfx.zx0" "..\RAM3\"
+move "ram4_gfx.zx0" "..\RAM4\"
+move "ram6_gfx.zx0" "..\RAM6\"
